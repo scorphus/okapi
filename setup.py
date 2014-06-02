93130b328a387c8001dceeae515a8b2b899c6dcf
gobind.ball <gobind.ball@addv3.redbeacon-inc.com>
2014-06-02 21:59:56 +0000

    Added wrapper for request library and store info in mongodb

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
+import os
 import sys
+import requests
 
 from setuptools import find_packages
+from setuptools import setup
 
 setup(
     name='okapi',
@@ -9,10 +12,16 @@ setup(
     description='Python Library to send API info to Storage Server',
     packages=find_packages(),
     classifiers=[
-        "Development Status :: 1 - Alpha",
-        "Environment :: Web Environment",
-        "Programming Language :: Python",
-        "Framework :: Django",
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Environment :: Web Environment',
+        'Programming Language :: Python',
     ],
     zip_safe=False,
+    install_requires = [
+        'pymongo',
+        'mongo',
+        'requests',
+    ],
+
 )
