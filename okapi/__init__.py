ea6403e0c71d8f967d97d9e37e4723aea39d65f8
Lorenzo Gil <lorenzo@redbeacon.com>
2014-06-25 02:41:03 +0000

    Add Redbeacon legal headers

--- okapi/__init__.py
+++ okapi/__init__.py
@@ -1,1 +1,9 @@
+# Copyright 2014 Red Beacon, Inc.  All Rights Reserved
+#
+# This code, and all derivative work, is the exclusive property of
+# Red Beacon, Inc. and may not be used without Red Beacon, Inc.'s
+# authorization.
+#
+# Author: Gobind Ball
+
 __version__ = '0.5'  
--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
+# Copyright 2014 Red Beacon, Inc.  All Rights Reserved
+#
+# This code, and all derivative work, is the exclusive property of
+# Red Beacon, Inc. and may not be used without Red Beacon, Inc.'s
+# authorization.
+#
+# Author: Gobind Ball
+
 from setuptools import find_packages
 from setuptools import setup
 
 setup(
     name='okapi',
     version='0.5',
-    author='RedBeacon',
+    author='RedBeacon (Gobind Ball)',
     author_email='support@redbeacon.com',
     description='Python Library to send API info to Storage Server',
     packages=find_packages(),
