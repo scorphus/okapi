40500eba326e2bcad824e7c36e10f44bd26f3dea
Lorenzo Gil <lorenzo@redbeacon.com>
2014-06-25 02:46:28 +0000

    Use spaces instead of tabs

--- okapi/api.py
+++ okapi/api.py
@@ -29,52 +29,52 @@ from pymongo import MongoClient
 
 class Api(object):
 
-	def __init__(self, project_name, host, port):
-		""" initialization of class api"""
-		self.host = host
-		self.port = port
-		self.project_name = project_name
+    def __init__(self, project_name, host, port):
+        """ initialization of class api"""
+        self.host = host
+        self.port = port
+        self.project_name = project_name
 
-		client = MongoClient(self.host, self.port)
-		self.db = client.okapi
+        client = MongoClient(self.host, self.port)
+        self.db = client.okapi
 
-	def request(self, method, url, **kwargs):
-		"""calls a method of request library while storing info about api call into mongo db"""
-		start = time.clock()
-		res = requests.request(method, url, **kwargs)
-		end = time.clock()
+    def request(self, method, url, **kwargs):
+        """calls a method of request library while storing info about api call into mongo db"""
+        start = time.clock()
+        res = requests.request(method, url, **kwargs)
+        end = time.clock()
 
-		content = ''
+        content = ''
 
-		if not res.ok:
-			content = res.content
+        if not res.ok:
+            content = res.content
 
-		date = datetime.datetime.utcnow()
-		host = urlparse.urlparse(res.url)
+            date = datetime.datetime.utcnow()
+            host = urlparse.urlparse(res.url)
 
-		data = {'content': content,
-				'date': date,
-				'host': host.hostname,
-			   	'method': method,
-			   	'project_name': self.project_name,
-			   	'response_time': (end - start),
-			   	'status_code': res.status_code,
-			   	'url': res.url,
-		}
+            data = {'content': content,
+                    'date': date,
+                    'host': host.hostname,
+                    'method': method,
+                    'project_name': self.project_name,
+                    'response_time': (end - start),
+                    'status_code': res.status_code,
+                    'url': res.url,
+            }
 
-		datas = self.db.datas
-		data_id = datas.insert(data)
+            datas = self.db.datas
+            data_id = datas.insert(data)
 
-		return res
+            return res
 
-	def get(self, url, **kwargs):
-		return self.request('GET', url, **kwargs)
+    def get(self, url, **kwargs):
+        return self.request('GET', url, **kwargs)
 
-	def delete(self, url, **kwargs):
-		return self.request('DELETE', url, **kwargs)
+    def delete(self, url, **kwargs):
+        return self.request('DELETE', url, **kwargs)
 
-	def post(self, url, **kwargs):
-		return self.request('POST', url, **kwargs)
+    def post(self, url, **kwargs):
+        return self.request('POST', url, **kwargs)
 
-	def put(self, url, **kwargs):
-		return self.request('PUT', url, **kwargs)
+    def put(self, url, **kwargs):
+        return self.request('PUT', url, **kwargs)
